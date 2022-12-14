dc4e66c948cbb2a58c321dab08ccd48a17d90247
Yarden Eshel <yardenyarden5@gmail.com>
2022-12-14 13:28:29 +0200

    all pieces have the move functions working

--- chess/code.js
+++ chess/code.js
@@ -64,6 +64,26 @@ function cellClick()
             {
                 isLegal = pawnMoveCheck(chosenCell,this);
             }
+            else if(backgroundPng.includes('knight'))
+            {
+                isLegal = knightMoveCheck(chosenCell,this);
+            }
+            else if(backgroundPng.includes('bishop'))
+            {
+                isLegal = bishopMoveCheck(chosenCell,this);
+            }
+            else if(backgroundPng.includes('rook'))
+            {
+                isLegal = rookMoveCheck(chosenCell,this);
+            }
+            else if(backgroundPng.includes('queen'))
+            {
+                isLegal = rookMoveCheck(chosenCell,this) || bishopMoveCheck(chosenCell,this);
+            }
+            else if(backgroundPng.includes('king'))
+            {
+                isLegal = kingMoveCheck(chosenCell,this)
+            }
             if(isLegal)
             {
                 this.style.backgroundImage = chosenCell.style.backgroundImage;
@@ -101,7 +121,7 @@ function cellClick()
         this.style.margin =  "0px";
     }
 }
-function  pawnMoveCheck(location, destination)//checks if the pawn can move or if its and illegal move
+function pawnMoveCheck(location, destination)//checks if the pawn can move or if its and illegal move
 {
     let position = parseInt(location.style["--boardIndex"],10);
     let future = parseInt(destination.style["--boardIndex"],10);
@@ -137,7 +157,7 @@ function  pawnMoveCheck(location, destination)//checks if the pawn can move or i
          {
             return 1;
          }
-         else if(destination.style.backgroundImage.includes("/w_")&& (position == future + 9 || position == future + 7))
+         else if(pieceOnDest.includes("/w_")&& (position == future + 9 || position == future + 7))
          {
             return 1;
          }
@@ -147,4 +167,108 @@ function  pawnMoveCheck(location, destination)//checks if the pawn can move or i
          }
     }
    
+}
+function knightMoveCheck(location,destination)
+{
+    let position = parseInt(location.style["--boardIndex"],10);
+    let future = parseInt(destination.style["--boardIndex"],10);
+    let pieceOnDest = destination.style.backgroundImage;
+    let colorCheck = (whiteToMove && pieceOnDest.includes('/b_') || !whiteToMove && pieceOnDest.includes("/w_") || pieceOnDest == "");//checks
+    let moveCheckPositive = (future == position + 6|| future == position + 10 || future == position + 17 || future == position + 15);//checks
+    let moveCheckNegative = (future == position - 6|| future == position - 10 || future == position - 17 || future == position - 15);//checks
+    let x = (future)%8
+    let y = (position)%8
+    let hacks = (((x == 0 || x == 1) && y > 6 )|| ((x == 6 || x == 7 )&& y < 2));
+
+    if(colorCheck && (moveCheckNegative||moveCheckPositive) && !hacks)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+function bishopMoveCheck(location,destination)
+{
+    let position = parseInt(location.style["--boardIndex"],10);// location in number/64
+    let future = parseInt(destination.style["--boardIndex"],10);// destination in number/64
+    let pieceOnDest = destination.style.backgroundImage; //if "" = no piece, if not then theres a piece there
+    let colorCheck = (whiteToMove && pieceOnDest.includes('/b_') || !whiteToMove && pieceOnDest.includes("/w_") || pieceOnDest == "");//checks
+    let diagonalAngle = 0;
+    let moveCheck = ((future - position) %  9 == 0 || (future - position) %  7 == 0);//check that the bishop move diagonaly
+    
+    if(colorCheck && moveCheck && destination.style.backgroundColor == location.style.backgroundColor)
+    {
+        if((future - position) %  9 == 0)
+        {
+            diagonalAngle = ((future - position)/Math.abs(future-position))*9;
+
+        }
+        else
+        {
+            diagonalAngle = ((future - position)/Math.abs(future-position))*7;
+        }
+        for(let i = position + diagonalAngle; i != future; i+= diagonalAngle)
+        {
+            if(container.childNodes[i].style.backgroundImage != "")
+            {
+                return 0;
+            }
+        }
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+function rookMoveCheck(location,destination)
+{
+    let position = parseInt(location.style["--boardIndex"],10);// location in number/64
+    let future = parseInt(destination.style["--boardIndex"],10);// destination in number/64
+    let pieceOnDest = destination.style.backgroundImage; //if "" = no piece, if not then theres a piece there
+    let colorCheck = (whiteToMove && pieceOnDest.includes('/b_') || !whiteToMove && pieceOnDest.includes("/w_") || pieceOnDest == "");//checks
+    let horOrVert = 0;
+    let moveCheck = ((future - position) %  8 == 0 || ((future/8) | 0) == ((position/8)| 0 ));//check that the rook moves straight
+    
+    if(colorCheck && moveCheck)
+    {
+        if(Math.abs(future - position) < 8)
+        {
+            diagonalAngle = ((future - position)/Math.abs(future-position));
+        }
+        else
+        {
+            diagonalAngle = ((future - position)/Math.abs(future-position))*8;
+        }
+        for(let i = position + diagonalAngle; i != future; i+= diagonalAngle)
+        {
+            if(container.childNodes[i].style.backgroundImage != "")
+            {
+                return 0;
+            }
+        }
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+function kingMoveCheck(location, destination)//checks if the king can move or if its an illegal move
+{
+    let position = parseInt(location.style["--boardIndex"],10);
+    let future = parseInt(destination.style["--boardIndex"],10);
+    let pieceOnDest = destination.style.backgroundImage;
+    let updownValid = position == future - 8 || position == future + 8;
+    let colorCheck = (whiteToMove && pieceOnDest.includes('/b_') || !whiteToMove && pieceOnDest.includes("/w_") || pieceOnDest == "")
+    let diagonalValid = (Math.abs(position - future) == 7 || Math.abs(position - future) == 9) && destination.style.backgroundColor == location.style.backgroundColor;
+    let sideValid = ((position)/8 | 0) == ((future)/8 | 0) && Math.abs(position - future) == 1;
+    if( (updownValid || diagonalValid || sideValid) && colorCheck)
+    {
+        return 1;
+    }
+    return 0;
 }
